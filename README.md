5b63c7f779db74f9fca4424f5c5d2b7cc64d8c79
LithaMangeni <litha.hashe.tlabs@gmail.com>
2023-11-10 15:50:40 +0200

    Completed IWA 19

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# LITMAN240_FTC2306_GROUPC_LITHAMANGENI_IWA19_book-connect
+
+## Book Connect 
+
+This library application caters to book enthusiasts, offering users previews of a diverse range of books. Additionally, it provides the flexibility to customize the interface mode, allowing users to choose between light and dark themes for enhanced comfort. The application further facilitates efficient book discovery through a search feature, enabling users to explore books based on genre, author, and title.
+
+## Features
+1. **Book Previews:** Users can access detailed previews for a wide array of books, providing insights into their content and context.
+
+2. **Search Functionality:** Users can efficiently search for specific books using various criteria, including genre, author, and book title.
+
+3. **Intuitive User Interface:** The application provides a user-friendly interface, ensuring a seamless and enjoyable experience for book lovers of all levels of familiarity with the platform.
+
+4. **Interactive Previews:** The book previews include interactive elements, such as images and descriptive details, enhancing the overall engagement and understanding of each book.
+
+5. **Mode Adjustment:** Users have the flexibility to adjust the app's interface mode, creating a visually comfortable environment for prolonged usage.
+
+## Requirements
+
+You need Visual Studio Code to access the code and a browser like Google Chrome to display the code.
+
+## User Stories
+
+1.As a user, I want to view a list of book previews, by title and author, so that I can discover new books to read.
+
+2.As a user, I want an image associated with all book previews so that I can recognize a book by the cover even if I forgot the name.
+
+3.As a user, I want to have the option of reading a summary of the book so that I can decide whether I want to read it.
+
+4.As a user, I want to have the option of seeing the date that a book was published so that I can determine how easy it is to obtain second-hand.
+
+5.As a user, I want to find books based on specific text phrases so that I don’t need to remember the entire title of a book.
+
+6.As a user, I want to filter books by author so that I can find books to read by authors that I enjoy.
+
+7.As a user, I want to filter books by genre so that I can find books to read in genres that I enjoy.
+
+8.As a user, I want to toggle between dark and light modes so that I can use the app comfortably at night.
--- scripts.js
+++ scripts.js
@@ -1,185 +1,222 @@
-matches = books
-page = 1;
+// Import constants and data from the "data.js" file
+import { BOOKS_PER_PAGE, authors, genres, books } from "./data.js";
 
-if (!books && !Array.isArray(books)) throw new Error('Source required') 
-if (!range && range.length < 2) throw new Error('Range must be an array with two numbers')
+// Copy the 'books' array to 'matches'
+const matches = books;
+// Set the initial page number to 1
+let page = 1;
 
-day = {
+// Check if 'books' is not defined or not an array, and throw an error if true
+if (!books || !Array.isArray(books)) throw new Error('Source required');
+
+// THEME
+// Define 'day' and 'night' themes as objects with dark and light color values
+const day = {
     dark: '10, 10, 20',
     light: '255, 255, 255',
-}
+};
 
-night = {
+const night = {
     dark: '255, 255, 255',
     light: '10, 10, 20',
+};
+
+// Select elements in the DOM related to theme settings
+const themeButton = document.querySelector('[data-header-settings]');
+const themeCancelButton = document.querySelector('[data-settings-cancel]');
+const settingsOverlay = document.querySelector("[data-settings-overlay]");
+const dataSettingsTheme = document.querySelector('[data-settings-theme]');
+const body = document.querySelector('body');
+
+// Function to apply a theme
+function applyTheme(theme) {
+    body.style.setProperty('--color-dark', theme.dark);
+    body.style.setProperty('--color-light', theme.light);
 }
 
-fragment = document.createDocumentFragment()
-const extracted = books.slice(0, 36)
+// Event listeners for showing and hiding theme settings
+themeButton.addEventListener('click', () => {
+    settingsOverlay.style.display = "block";
+});
 
-for ({ author, image, title, id }; extracted; i++) {
-    const preview = createPreview({
-        author,
-        id,
-        image,
-        title
-    })
+themeCancelButton.addEventListener('click', () => {
+    settingsOverlay.style.display = "none";
+});
 
-    fragment.appendChild(preview)
-}
+// 'saveButton' variable holds a reference to the save button element.
+const saveButton = document.querySelector('[type="submit"][form="settings"]');
 
-data-list-items.appendChild(fragment)
+// Event listener for the save button in theme settings
+saveButton.addEventListener('click', (event) => {
+    event.preventDefault();
 
-genres = document.createDocumentFragment()
-element = document.createElement('option')
-element.value = 'any'
-element = 'All Genres'
-genres.appendChild(element)
-
-for ([id, name]; Object.entries(genres); i++) {
-    document.createElement('option')
-    element.value = value
-    element.innerText = text
-    genres.appendChild(element)
-}
-
-data-search-genres.appendChild(genres)
-
-authors = document.createDocumentFragment()
-element = document.createElement('option')
-element.value = 'any'
-element.innerText = 'All Authors'
-authors.appendChild(element)
-
-for ([id, name];Object.entries(authors); id++) {
-    document.createElement('option')
-    element.value = value
-    element = text
-    authors.appendChild(element)
-}
-
-data-search-authors.appendChild(authors)
-
-data-settings-theme.value === window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
-v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches? 'night' | 'day'
-
-documentElement.style.setProperty('--color-dark', css[v].dark);
-documentElement.style.setProperty('--color-light', css[v].light);
-data-list-button = "Show more (books.length - BOOKS_PER_PAGE)"
-
-data-list-button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
-
-data-list-button.innerHTML = /* html */ [
-    '<span>Show more</span>',
-    '<span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>',
-]
-
-data-search-cancel.click() { data-search-overlay.open === false }
-data-settings-cancel.click() { querySelect(data-settings-overlay).open === false }
-data-settings-form.submit() { actions.settings.submit }
-data-list-close.click() { data-list-active.open === false }
-
-data-list-button.click() {
-    document.querySelector([data-list-items]).appendChild(createPreviewsFragment(matches, page x BOOKS_PER_PAGE, {page + 1} x BOOKS_PER_PAGE]))
-    actions.list.updateRemaining()
-    page = page + 1
-}
-
-data-header-search.click() {
-    data-search-overlay.open === true ;
-    data-search-title.focus();
-}
-
-data-search-form.click(filters) {
-    preventDefault()
-    const formData = new FormData(event.target)
-    const filters = Object.fromEntries(formData)
-    result = []
-
-    for (book; booksList; i++) {
-        titleMatch = filters.title.trim() = '' && book.title.toLowerCase().includes[filters.title.toLowerCase()]
-        authorMatch = filters.author = 'any' || book.author === filters.author
-
-        {
-            genreMatch = filters.genre = 'any'
-            for (genre; book.genres; i++) { if singleGenre = filters.genre { genreMatch === true }}}
-        }
-
-        if titleMatch && authorMatch && genreMatch => result.push(book)
+    // Apply theme based on the selected option
+    if (dataSettingsTheme.value === 'day') {
+        applyTheme(day);
+    } else if (dataSettingsTheme.value === 'night') {
+        applyTheme(night);
     }
 
-    if display.length < 1 
-    data-list-message.class.add('list__message_show')
-    else data-list-message.class.remove('list__message_show')
-    
+    // Hide the theme settings overlay
+    settingsOverlay.style.display = "none";
+});
 
-    data-list-items.innerHTML = ''
-    const fragment = document.createDocumentFragment()
-    const extracted = source.slice(range[0], range[1])
+// Initially, apply the "day" theme
+applyTheme(day);
 
-    for ({ author, image, title, id }; extracted; i++) {
-        const { author: authorId, id, image, title } = props
+// Clear the existing content in the data-list-items element
+document.querySelector('[data-list-items]').innerHTML = '';
+// Create a document fragment to hold preview elements
+const fragment = document.createDocumentFragment();
+// Set the initial start and end indices for book extraction
+let startIndex = 0;
+let endIndex = 36;
+// Extract a subset of books based on the start and end indices
+const extracted = books.slice(startIndex, endIndex);
 
-        element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
+// Iterate over the extracted books and create preview elements
+for (const { author, image, title, id, description, published, genres } of extracted) {
+    const preview = document.createElement('dl');
+    preview.className = 'preview';
+    preview.dataset.id = id;
+    preview.dataset.title = title;
+    preview.dataset.image = image;
+    preview.dataset.subtitle = `${authors[author]} (${(new Date(published)).getFullYear()})`;
+    preview.dataset.description = description;
+    preview.dataset.genre = genres;
 
-        element.innerHTML = /* html */ `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[authorId]}</div>
+    // Create the preview's HTML structure using innerHTML and append the preview to the fragment
+    preview.innerHTML = /*html*/ `
+        <div>
+            <image class='preview__image' src="${image}" alt="book pic"/>
+        </div>
+        <div class='preview__info'>
+            <dt class='preview__title'>${title}</dt>
+            <dt class='preview__author'> By ${authors[author]}</dt>
+        </div>`;
+    fragment.appendChild(preview);
+}
+
+// Select the element that will contain the book previews
+const booklist1 = document.querySelector('[data-list-items]');
+// Append all preview elements to the container
+booklist1.appendChild(fragment);
+
+// SEARCH BUTTON
+// Select the search button element
+const searchbutton = document.querySelector("[data-header-search]");
+// Event listener for displaying the search overlay
+searchbutton.addEventListener('click', () => {
+    document.querySelector("[data-search-overlay]").style.display = "block";
+});
+
+// 'searchCancel' variable holds a reference to the cancel search button element.
+const searchCancel = document.querySelector("[data-search-cancel]");
+// Event listener for hiding the search overlay
+searchCancel.addEventListener('click', () => {
+    document.querySelector("[data-search-overlay]").style.display = "none";
+});
+
+// Create options for authors in the search select element
+const authorSelect = document.querySelector("[data-search-authors]");
+for (const authorId in authors) {
+    const optionElement = document.createElement('option');
+    optionElement.value = authorId;
+    optionElement.textContent = authors[authorId];
+    authorSelect.appendChild(optionElement);
+}
+
+// Create options for genres in the search select element
+const genreSelect = document.querySelector("[data-search-genres]");
+for (const genreId in genres) {
+    const optionElement = document.createElement('option');
+    optionElement.value = genreId;
+    optionElement.textContent = genres[genreId];
+    genreSelect.appendChild(optionElement);
+}
+
+// CODE DISPLAYS THE BOOK DETAILS
+// Select the element that contains book previews
+const detailsContainer = document.querySelector('[data-list-items]');
+// Select elements related to book details
+const overlay = document.querySelector('[data-list-active]');
+const title = document.querySelector('[data-list-title]');
+const subtitle = document.querySelector('[data-list-subtitle]');
+const description = document.querySelector('[data-list-description]');
+const image1 = document.querySelector('[data-list-image]');
+const imageblur = document.querySelector('[data-list-blur]');
+const detailsClose = document.querySelector('[data-list-close]');
+
+// Event listener using event delegation for showing book details
+detailsContainer.addEventListener('click', (event) => {
+    const clickedElement = event.target.closest('.preview');
+
+    if (clickedElement) {
+        overlay.style.display = 'block';
+        description.innerHTML = clickedElement.dataset.description || '';
+        subtitle.innerHTML = clickedElement.dataset.subtitle || '';
+        title.innerHTML = clickedElement.dataset.title || '';
+        image1.setAttribute('src', clickedElement.dataset.image || '');
+        imageblur.setAttribute('src', clickedElement.dataset.image || '');
+    }
+});
+
+// Event listener for hiding book details
+detailsClose.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
+// Select the "Show More" button element
+const showMoreButton = document.querySelector('[data-list-button]');
+
+// Function to update the text content of the "Show More" button
+function updateShowMoreButtonText() {
+    const numItemsToShow = Math.min(books.length - endIndex);
+    showMoreButton.textContent = `Show More (${numItemsToShow})`;
+}
+
+// Function to append previews to the fragment based on the updated indices
+function appendPreviewsToFragment(startIndex, endIndex) {
+    const fragment = document.createDocumentFragment();
+    const extracted = books.slice(startIndex, endIndex);
+
+    for (const { author, image, title, id, description, published } of extracted) {
+        const preview = document.createElement('dl');
+        preview.className = 'preview';
+        preview.dataset.id = id;
+        preview.dataset.title = title;
+        preview.dataset.image = image;
+        preview.dataset.subtitle = `${authors[author]} (${(new Date(published)).getFullYear()})`;
+        preview.dataset.description = description;
+
+        preview.innerHTML = /*html*/ `
+            <div>
+                <image class='preview__image' src="${image}" alt="book pic"/>
             </div>
-        `
-
-        fragment.appendChild(element)
+            <div class='preview__info'>
+                <dt class='preview__title'>${title}</dt>
+                <dt class='preview__author'> By ${authors[author]}</dt>
+            </div>`;
+        fragment.appendChild(preview);
     }
-    
-    data-list-items.appendChild(fragments)
-    initial === matches.length - [page * BOOKS_PER_PAGE]
-    remaining === hasRemaining ? initial : 0
-    data-list-button.disabled = initial > 0
 
-    data-list-button.innerHTML = /* html */ `
-        <span>Show more</span>
-        <span class="list__remaining"> (${remaining})</span>
-    `
-
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    data-search-overlay.open = false
+    const booklist1 = document.querySelector('[data-list-items]');
+    booklist1.appendChild(fragment);
 }
 
-data-settings-overlay.submit; {
-    preventDefault()
-    const formData = new FormData(event.target)
-    const result = Object.fromEntries(formData)
-    document.documentElement.style.setProperty('--color-dark', css[result.theme].dark);
-    document.documentElement.style.setProperty('--color-light', css[result.theme].light);
-    data-settings-overlay).open === false
-}
+// Event listener for the "Show More" button
+showMoreButton.addEventListener('click', () => {
+    // Update start and end indices
+    startIndex += 36;
+    endIndex += 36;
 
-data-list-items.click() {
-    pathArray = Array.from(event.path || event.composedPath())
-    active;
+    // Update the text content of the "Show More" button
+    updateShowMoreButtonText();
 
-    for (node; pathArray; i++) {
-        if active break;
-        const previewId = node?.dataset?.preview
-    
-        for (const singleBook of books) {
-            if (singleBook.id === id) active = singleBook
-        } 
-    }
-    
-    if !active return
-    data-list-active.open === true
-    data-list-blur + data-list-image === active.image
-    data-list-title === active.title
-    
-    data-list-subtitle === '${authors[active.author]} (${Date(active.published).year})'
-    data-list-description === active.description
-}
+    // Append previews to the fragment based on the updated indices
+    appendPreviewsToFragment(startIndex, endIndex);
+});
+
+// Initial setup
+updateShowMoreButtonText();
+appendPreviewsToFragment(startIndex, endIndex);
